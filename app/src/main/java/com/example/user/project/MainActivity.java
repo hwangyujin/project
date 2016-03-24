59ebf5b62904b5f228080cc1a96134ce72259dcf
hwangyujin <hyhyhhyy@naver.com>
2016-03-24 21:20:02 +0900

    start create new android project

--- app/src/main/java/com/example/user/project/MainActivity.java
+++ app/src/main/java/com/example/user/project/MainActivity.java
@@ -2,9 +2,10 @@ package com.example.user.project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
